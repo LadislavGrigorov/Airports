3df1d7e30512da1a4c829555c9742a8f62254ea5
Daniel-Todorov <danieltodorov2013@gmail.com>
2014-09-03 13:08:03 +0300

    Fixed a typo in the xml folder path

--- source/Airports/Airports.Client/Program.cs
+++ source/Airports/Airports.Client/Program.cs
@@ -18,33 +18,33 @@
         private const string ExcelReportsDestinationPath = @"..\..\..\..\exports\Excel-Reports\";
         private const string PdfReportsFolderPath = @"..\..\..\..\exports\PDF-Reports\";
         private const string PdfReportsFileName = @"flight-report.pdf";
-        private const string XmlReportsFolderPath = @"..\..\..\..\Exports\Xml-Reports\";
+        private const string XmlReportsFolderPath = @"..\..\..\..\exports\Xml-Reports\";
 
         static void Main()
         {
             IAirportsDataSqlServer airportsData = new AirportsDataSqlServer();
             IAirportsDataMongoDb mongoData = new AirportsDataMongoDb();
             
-            /*Task 1:
-             * a) Extract *.xls and *.xlsx files from a zip archive; read and load the data into SQL Server.
-             * b) Import data from MongoDb to SQL Server. */
-            //ExtractZipAndImportDataFromExcelAndMongoDb(airportsData, mongoData);
+            ///*Task 1:
+            // * a) Extract *.xls and *.xlsx files from a zip archive; read and load the data into SQL Server.
+            // * b) Import data from MongoDb to SQL Server. */
+            ////ExtractZipAndImportDataFromExcelAndMongoDb(airportsData, mongoData);
             
-            //Task 2: Generate PDF Reports
-            GeneratePdfFlightsReport(airportsData);
+            ////Task 2: Generate PDF Reports
+            //GeneratePdfFlightsReport(airportsData);
 
             //Tast 3: Generate report in XML format 
             GenerateXmlFlightsReport(airportsData);
 
-            // Task 4: a) Genetare JSON reports from SQL Server to file system.
-            //         b) Import reports from file system (.json files) to MySQL.
-            GenerateJsonFlightsReportsAndLoadToMySql(airportsData);
+            //// Task 4: a) Genetare JSON reports from SQL Server to file system.
+            ////         b) Import reports from file system (.json files) to MySQL.
+            //GenerateJsonFlightsReportsAndLoadToMySql(airportsData);
 
-            /* Task 5: Load Data from XML and save it in SQL Server and MongoDb */
-            ImportFlightsDataFromXmlAndLoadToMongoDb(airportsData, mongoData);
+            ///* Task 5: Load Data from XML and save it in SQL Server and MongoDb */
+            //ImportFlightsDataFromXmlAndLoadToMongoDb(airportsData, mongoData);
 
-            /* Task 6: Export merged report from MySql and SQLite to Excel 2007 file */
-            //ExcelReportExporter.GenerateExcelFile(ExcelReportsFolderPath);
+            ///* Task 6: Export merged report from MySql and SQLite to Excel 2007 file */
+            ////ExcelReportExporter.GenerateExcelFile(ExcelReportsFolderPath);
         }
 
         private static void ExtractZipAndImportDataFromExcelAndMongoDb(IAirportsDataSqlServer airportsData, IAirportsDataMongoDb mongoData)
