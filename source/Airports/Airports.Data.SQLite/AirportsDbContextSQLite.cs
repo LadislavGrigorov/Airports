aabf16e0a473d25eca6d640589b5577e6a016429
Radoslav Chervenkov <chervenkovr@gmail.com>
2014-09-03 19:04:52 +0300

    Partial SQLite functionality

---
+++ source/Airports/Airports.Data.SQLite/AirportsDbContextSQLite.cs
@@ -0,0 +1,14 @@
+﻿namespace Airports.Data.SQLite
+{
+    using System.Data.Entity;
+    using Airports.Models.SQLite;
+
+    public class AirportsDbContextSQLite : DbContext
+    {
+        public AirportsDbContextSQLite() : base("")
+        {
+        }
+
+        public IDbSet<Airline> Airlines { get; set; }
+    }
+}
---
+++ source/Airports/Airports.Models.SQLite/Airline.cs
@@ -0,0 +1,16 @@
+﻿namespace Airports.Models.SQLite
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Airline
+    {
+        [Key]
+        public long AirlineId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Website { get; set; }
+
+        public string FoundationYear { get; set; }
+    }
+}
